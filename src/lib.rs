6bea54a4ae02b6683169ee632056c966d18d91d5
ribhuhooja <ribhu.hooja.27@dartmouth.edu>
2024-03-31 22:18:44 -0400

    started implementing the frame struct

--- src/lib.rs
+++ src/lib.rs
@@ -4,14 +4,61 @@
 //! julia sets defined on different coordinates, and provides
 //! functionality to zoom
 
-use num::complex::Complex;
+use num::{complex::Complex, range};
 
 trait Frame {
     // get pixel rgb value
     // sliceablity
 }
 
-struct JuliaFrame<'a> {
+pub struct JuliaFrame {
     values: Vec<Complex<f32>>,
-    coordinates: &'a Vec<Complex<f32>>,
+    coordinates: Vec<Complex<f32>>, // TODO: Make this a reference for optimizing space
+    center: Complex<f32>,
+    julia_number: Complex<f32>,
+    width: u32,
+    height: u32,
+    cell_width: f32,
+    cell_height: f32,
+}
+
+impl JuliaFrame {
+    fn initial_values(
+        center: Complex<f32>,
+        width: u32,
+        height: u32,
+        cell_width: f32,
+        cell_height: f32,
+    ) -> Vec<Complex<f32>> {
+        let mut result = Vec::new();
+
+        for _ in range(0, width * height) {
+            result.push(Complex::new(0.0, 0.0));
+        }
+
+        result
+    }
+}
+
+impl Default for JuliaFrame {
+    fn default() -> Self {
+        let center = Complex { re: 0.0, im: 0.0 };
+        let julia_number = Complex { re: 0.0, im: 0.0 };
+        let (width, height) = (800, 600);
+        let (cell_width, cell_height) = (0.005, 0.005);
+        let coordinates =
+            JuliaFrame::initial_values(center, width, height, cell_width, cell_height);
+        let values = JuliaFrame::initial_values(center, width, height, cell_width, cell_height);
+
+        Self {
+            center,
+            julia_number,
+            width,
+            height,
+            cell_width,
+            cell_height,
+            coordinates,
+            values,
+        }
+    }
 }
