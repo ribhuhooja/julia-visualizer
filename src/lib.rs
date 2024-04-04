32b6aa4a771c5eabc3bdf2858219be9456ac74c9
ribhuhooja <ribhu.hooja.27@dartmouth.edu>
2024-04-04 02:53:53 -0400

    Fixed the Julia datastructure

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,11 @@
 //! julia sets defined on different coordinates, and provides
 //! functionality to zoom
 
-use num::{complex::Complex, range};
+use num::complex::Complex;
 
-trait Frame {
+const ESCAPE_VALUE: f32 = 2.0 * 2.0;
+
+pub trait Frame {
     // get pixel rgb value
     // sliceablity
 }
@@ -23,6 +25,34 @@ pub struct JuliaFrame {
 }
 
 impl JuliaFrame {
+    pub fn new(
+        center: Complex<f32>,
+        width: u32,
+        height: u32,
+        cell_width: f32,
+        cell_height: f32,
+        julia_number: Complex<f32>,
+    ) -> Self {
+        let mut result = Self {
+            center,
+            width,
+            height,
+            cell_width,
+            cell_height,
+            julia_number,
+            ..Default::default()
+        };
+
+        result.initialize();
+        result
+    }
+
+    pub fn into_recentered(mut self, center: Complex<f32>, julia_number: Complex<f32>) -> Self {
+        (self.center, self.julia_number) = (center, julia_number);
+        self.initialize();
+        self
+    }
+
     fn initial_values(
         center: Complex<f32>,
         width: u32,
@@ -32,11 +62,85 @@ impl JuliaFrame {
     ) -> Vec<Complex<f32>> {
         let mut result = Vec::new();
 
-        for _ in range(0, width * height) {
-            result.push(Complex::new(0.0, 0.0));
+        let top_left_corner = Complex::new(
+            center.re - (width as f32) * cell_width / 2.0,
+            center.im + (height as f32) * cell_height / 2.0,
+        );
+
+        for j in 0..height {
+            for i in 0..width {
+                let real_part = top_left_corner.re + (i as f32) * cell_width;
+                let imaginary_part = top_left_corner.im - (j as f32) * cell_height;
+                result.push(Complex::new(real_part, imaginary_part));
+            }
         }
 
         result
+    }
+
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    fn initialize(&mut self) {
+        let coordinates = Self::initial_values(
+            self.center,
+            self.width,
+            self.height,
+            self.cell_width,
+            self.cell_height,
+        );
+
+        let values = coordinates.clone();
+
+        self.values = values;
+        self.coordinates = coordinates;
+    }
+
+    pub fn iterate_escape_time(&mut self, num_iters: u32) {
+        for value_ref in &mut self.values {
+            for _ in 0..num_iters {
+                let value = *value_ref;
+                if value.norm_sqr() > ESCAPE_VALUE {
+                    break;
+                }
+                *value_ref = value * value + self.julia_number;
+            }
+        }
+    }
+
+    //TODO: remove this
+    pub fn debug_iterate_escape_time(&mut self, num_iters: u32, x: u32, y: u32) {
+        let i = Self::index_of(x, y, self.width);
+
+        for _ in 0..num_iters {
+            let value = self.values[i as usize];
+            if value.norm_sqr() > ESCAPE_VALUE {
+                break;
+            }
+            let value = value * value + self.julia_number;
+            self.values[i as usize] = value;
+            println!("{value}");
+        }
+
+        println!("{}", self.values[i as usize]);
+    }
+
+    pub fn value_at(&self, i: u32, j: u32) -> Option<&Complex<f32>> {
+        self.values.get(Self::index_of(i, j, self.width) as usize)
+    }
+
+    pub fn has_escaped(&self, i: u32, j: u32) -> Option<bool> {
+        let val = self.value_at(i, j)?;
+        Some(val.norm_sqr() > ESCAPE_VALUE)
+    }
+
+    fn index_of(i: u32, j: u32, width: u32) -> u32 {
+        width * j + i
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+use image::{Rgb, RgbImage};
+use julia_visualizer::JuliaFrame;
+use num::complex::Complex;
+
 fn main() {
-    println!("Hello, world!");
+    let mut frame = JuliaFrame::default();
+    let center = Complex::new(0.0, 0.0);
+    let julia_number = Complex::new(0.35, 0.35);
+
+    frame = frame.into_recentered(center, julia_number);
+
+    frame.iterate_escape_time(500);
+
+    let width = frame.width();
+    let height = frame.height();
+
+    let mut img = RgbImage::new(width, height);
+
+    for x in 0..width {
+        for y in 0..height {
+            let pixel = if frame.has_escaped(x, y).unwrap() {
+                Rgb([255, 255, 255])
+            } else {
+                Rgb([0, 0, 0])
+            };
+
+            // let val = frame.value_at(x, y).unwrap().norm_sqr();
+            // let color_val: u32 = (255.0 * val / 8.0) as u32;
+            // let color_val: u8 = color_val.try_into().unwrap();
+            // let pixel = Rgb([color_val, color_val, color_val]);
+
+            // println!("{}", color_val);
+
+            img.put_pixel(x, y, pixel)
+        }
+    }
+
+    let res = img.save("output.png");
+    match res {
+        Ok(_) => println!("saved!"),
+        Err(err) => println!("not saved :( because of error:\n{err}"),
+    }
 }
